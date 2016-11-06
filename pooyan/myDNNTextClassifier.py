89bf951e337f9c5ee78887b873f26f6f9fc4439e
pooyan <behnamghader@gmail.com>
2016-11-05 20:14:12 -0700

    prediction test

--- pooyan/myDNNTextClassifier.py
+++ pooyan/myDNNTextClassifier.py
@@ -29,9 +29,9 @@ classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
                                             hidden_units=[10, 20, 10],
                                             n_classes=3,
                                             model_dir="/tmp/spambase_model")
-print (X_train[0])
+# print (X_train[0])
 print (len(y_train))
-print (X_test[0])
+# print (X_test[0])
 print (len(y_test))
 
 # Fit model.
@@ -43,4 +43,14 @@ classifier.fit(x=X_train,
 accuracy_score = classifier.evaluate(x=X_test,
                                      y=y_test)["accuracy"]
 print('Accuracy: {0:f}'.format(accuracy_score))
+
+
+# Classify a new email
+new_samples = np.array(
+    [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4.83,4.83,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.302,0,1.7,5,17]
+     ,[0,0,0,0,0,0,0,0,0,0,0,1.53,0,0,0,0,0,0,4.61,0,0,0,0,0,0,0,0,0,1.53,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2.222,5,20]], dtype=float)
+y = list(classifier.predict(new_samples, as_iterable=True))
+print('Predictions: {}'.format(str(y)))
+
+
 
